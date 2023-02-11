681c34094723aa8989b7943b6f7226aba93c0765
davidcoats <david@getsafetycone.com>
2023-02-10 17:42:13 -0800

    Interim changes.

--- source/R5T.F0003/Code/Extensions/SyntaxTokenExtensions-Trivia.cs
+++ source/R5T.F0003/Code/Extensions/SyntaxTokenExtensions-Trivia.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.Magyar;
 
 
 namespace System
